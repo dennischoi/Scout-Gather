1b7a4d9e268268164b16f9d2ca36e164a5de4387
Dennis Choi <dennis.choi7@gmail.com>
2017-06-07 23:15:55 -0400

    components laid out proper for fresh project

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './app/components/App';
 import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import './app/styles/css/index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
