c0be839c7dcdd2362d8a8635d76a376bead89844
Dennis Choi <dennis.choi7@gmail.com>
2017-06-13 19:12:11 -0400

    base footer template

--- src/app/components/Footer/Footer.js
+++ src/app/components/Footer/Footer.js
@@ -9,7 +9,20 @@ class Footer extends Component {
   render() {
     return (
       <div className="footerMain">
-        
+        <section className="footerContainer">
+        	<div className="footerItems">
+        		<h3>Scout & Gather</h3>
+        	</div>
+        	<div className="footerItems">
+        		<h3>Media</h3>
+        	</div>
+        	<div className="footerItems">
+        		<h3></h3>
+        	</div>
+        	<div className="footerItems">
+        		<h3>Contact Us</h3>
+        	</div>
+        </section>
       </div>
     );
   } // Render
