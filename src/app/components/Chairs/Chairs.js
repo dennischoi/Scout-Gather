d680420129275fdf006c9e7c987de6a9f1d8494d
Dennis Choi <dennis.choi7@gmail.com>
2017-06-15 21:51:24 -0400

    test

---
+++ src/app/components/Chairs/Chairs.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+// Import Components
+import ChairsUpdate from './ChairsUpdate/ChairsUpdate';
+
+class Chairs extends Component {
+  render() {
+    return (
+      <div className="Chair-Main">
+        <ChairsUpdate />
+      </div>
+    );
+  }
+}
+
+export default Chairs;
