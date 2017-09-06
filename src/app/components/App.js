619c2aa0b0f8fb55ff108b39a1ba5c23a277efc5
Dennis Choi <dennis.choi7@gmail.com>
2017-09-06 10:08:32 -0400

    Test routing

--- src/app/components/App.js
+++ src/app/components/App.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+
 // Import Components
-import Header from './Header/Header';
+// import Header from './Header/Header';
 import Home from './Home/Homepage';
 import Featured from './Featured/Featured';
 import Footer from './Footer/Footer';
@@ -15,7 +21,21 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
+        {/*<Header />*/}
+        <Router>
+          <div>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/chairs">Chairs</Link></li>
+            </ul>
+
+            <hr/>
+
+            <Route exact path="/" component={Home} />
+            <Route path="/chairs" component={Featured} />
+          </div>
+        </Router>
+
         <div className="App-Home">
           <img src={logoBg} className="appBg" alt="logo" />
         </div>
@@ -23,7 +43,9 @@ class App extends Component {
           <Home />
         </div>
         <div>
-          <Featured />
+
+
+          {/*<Featured />*/}
         </div>
         <Footer />
       </div>
